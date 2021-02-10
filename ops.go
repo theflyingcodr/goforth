6530b57999b2e72a07cb15f5dd35f7658469d565
Mark Smith <mark@theflyingcodr.dev>
2021-02-10 09:25:34 +0000

    adding comments and a new duplicate operator

--- ops.go
+++ ops.go
@@ -8,14 +8,20 @@ import (
 	"github.com/labstack/gommon/log"
 )
 
+// ops is a list of all supported operations and their function for easy execution.
 var ops = map[string]func(s *stackWrapper){
 	"ADD":     add,
 	"SUB":     sub,
 	"EQ":      equal,
 	"NE":      notequal,
 	"HASH256": hash256,
+	"DUP":     duplicate,
 }
 
+// add will add two numbers together by popping
+// them off the stack, converting to int and
+// performing the maths.
+// It will then push the result onto the stack.
 func add(stack *stackWrapper) {
 	a := stack.pop()
 	b := stack.pop()
@@ -31,6 +37,12 @@ func add(stack *stackWrapper) {
 	stack.push(fmt.Sprintf("%d", o))
 }
 
+// sub will minus two numbers together by popping
+// them off the stack, converting to int and
+// performing the maths.
+// This will subtract in the order of the expression, so,
+// given 10 3 SUB it will perform 10-3.
+// It will then push the result onto the stack.
 func sub(stack *stackWrapper) {
 	a := stack.pop()
 	b := stack.pop()
@@ -46,6 +58,9 @@ func sub(stack *stackWrapper) {
 	stack.push(fmt.Sprintf("%d", o))
 }
 
+// equal will check the last to items on the stack are equal.
+// It will return 1 if TRUE and 0 if FALSE.
+// The result will be pushed onto the stack.
 func equal(stack *stackWrapper) {
 	if stack.pop() == stack.pop() {
 		stack.push("1")
@@ -54,6 +69,9 @@ func equal(stack *stackWrapper) {
 	stack.push("0")
 }
 
+// notequal will check the last to items on the stack are not equal.
+// It will return 1 if TRUE and 0 if FALSE.
+// The result will be pushed onto the stack.
 func notequal(stack *stackWrapper) {
 	if stack.pop() != stack.pop() {
 		stack.push("1")
@@ -62,8 +80,19 @@ func notequal(stack *stackWrapper) {
 	stack.push("0")
 }
 
+// hash256 will hash the current top value in the stack
+// using the sha256 algo.
+// The result is pushed onto the stack.
 func hash256(stack *stackWrapper) {
 	s := stack.pop()
 	h := sha256.Sum256([]byte(s))
 	stack.push(fmt.Sprintf("%x", h))
+}
+
+// duplicate will take the top item in the stack and duplicate it.
+// It will then push this duplicate value to the top of the stack.
+func duplicate(stack *stackWrapper) {
+	s := stack.pop()
+	stack.push(s)
+	stack.push(s)
 }
